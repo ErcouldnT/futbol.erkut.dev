9ee76cd2a79d72dc2caddb2b22c98033b210c822
Ercode Techoglu <ercode@pm.me>
2025-03-28 02:00:57 +0300

    add: seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,19 @@
 
 <svelte:head>
 	<title>Akkuyu Futbol</title>
+	<meta name="description" content="Akkuyu Futbol - Kadro oluşturucu." />
+	<meta name="keywords" content="football, Akkuyu, sports, news, scores, updates" />
+	<meta name="author" content="Ercode Tech" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta property="og:title" content="Akkuyu Futbol" />
+	<meta property="og:description" content="Akkuyu Futbol - Kadro oluşturucu." />
+	<meta property="og:image" content="/favicon.png" />
+	<meta property="og:url" content="https://futbol.erkut.dev" />
+	<meta property="og:type" content="website" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Akkuyu Futbol" />
+	<meta name="twitter:description" content="Akkuyu Futbol - Kadro oluşturucu." />
+	<meta name="twitter:image" content="/favicon.png" />
 </svelte:head>
 
 <div class="mx-auto max-w-7xl">
