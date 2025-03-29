12d72014ae97ebf4f4c250bb7d74f92647df24cd
Semih <semih.bolukbasi@yandex.com>
2025-03-29 18:15:39 +0300

    update: mobile version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,26 +31,22 @@
 	});
 </script>
 
-<div class="flex flex-col items-center gap-6 px-31">
+<div class="flex flex-col items-center gap-6 px-4 sm:px-8 lg:px-31">
 	{#each matches as match}
 		<div class="card bg-base-300 w-full max-w-4xl cursor-pointer shadow-xl">
 			<div class="card-body">
 				<h2 class="card-title text-primary">{match.date}</h2>
 				<p class="text-success text-xs">{match.hour}</p>
-				<div class="flex items-start justify-between">
+				<div class="flex flex-wrap items-start justify-between">
 					<!-- Home Team Section -->
-					<div class="flex min-h-[20px] w-1/2 flex-col items-center">
+					<div class="flex min-h-[20px] w-full flex-col items-center sm:w-1/2 lg:w-1/4">
 						<div class="collapse-arrow collapse w-full">
 							<input type="checkbox" class="peer" />
 							<div class="collapse-title p-3.5 text-center text-sm font-semibold">
-								<!-- Reduced padding -->
 								<span class="badge badge-primary mt-1">{match.homeTeam}</span>
-								<!-- Added margin-top to bring closer to the title -->
 							</div>
 							<div class="collapse-content p-2">
-								<!-- Adjusted padding -->
 								<ul class="list-none text-center text-sm leading-tight">
-									<!-- Adjusted text size -->
 									{#each match.homeTeamPlayers as player}
 										<li class="text-primary">{player}</li>
 									{/each}
@@ -60,31 +56,21 @@
 					</div>
 
 					<!-- Match Score and Goals Section -->
-					<div class="mt-2 flex w-1/4 flex-col items-center text-center">
-						<!-- Match Score -->
+					<div class="mt-2 flex w-full flex-col items-center text-center sm:w-1/2 lg:w-1/4">
 						<p class="text-4xl font-bold">{match.score}</p>
-
-						<!-- Goals Section -->
 						<div class="mt-2 flex flex-col items-center gap-1">
-							<!-- Reduced gap -->
 							<div class="collapse-arrow collapse w-full">
 								<input type="checkbox" class="peer" />
 								<div class="collapse-title text-base-content p-4.5 text-sm font-semibold">
-									<!-- Adjusted padding -->
 									Goller
 								</div>
 								<div class="collapse-content p-1">
-									<!-- Adjusted padding -->
 									<div class="flex justify-between gap-1 text-xs">
-										<!-- Reduced gap -->
-										<!-- Home Team Goals -->
 										<ul class="text-primary list-none text-left text-[0.75rem] leading-tight">
 											{#each match.goalsHome as goal}
 												<li class="truncate">{goal.player} ({goal.goalNumber})</li>
 											{/each}
 										</ul>
-
-										<!-- Away Team Goals -->
 										<ul class="text-secondary list-none text-right text-[0.75rem] leading-tight">
 											{#each match.goalsAway as goal}
 												<li class="truncate">{goal.player} ({goal.goalNumber})</li>
@@ -97,17 +83,14 @@
 					</div>
 
 					<!-- Away Team Section -->
-					<div class="flex min-h-[20px] w-1/2 flex-col items-center">
+					<div class="flex min-h-[20px] w-full flex-col items-center sm:w-1/2 lg:w-1/4">
 						<div class="collapse-arrow collapse w-full">
 							<input type="checkbox" class="peer" />
 							<div class="collapse-title p-3.5 text-center text-sm font-semibold">
-								<!-- Adjusted padding -->
 								<span class="badge badge-secondary mt-1">{match.awayTeam}</span>
 							</div>
 							<div class="collapse-content p-2">
-								<!-- Adjusted padding -->
 								<ul class="list-none text-center text-sm leading-tight">
-									<!-- Adjusted text size -->
 									{#each match.awayTeamPlayers as player}
 										<li class="text-secondary">{player}</li>
 									{/each}
