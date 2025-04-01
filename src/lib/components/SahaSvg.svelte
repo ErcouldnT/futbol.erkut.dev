9c8fde9e090463d4965a87fd6d3f05362c88386e
Semih <75166674+SemihBolukbasi@users.noreply.github.com>
2025-04-01 23:34:55 +0300

    add: tabs (#12)
    * change: skoreboard, teams, goals
    
    * Update supabase.ts
    
    Production secret lar local secret tan farklı
    
    * update: mobile version
    
    * change: goaler players
    
    * format: string ler tek tırnak yerine çift tırnak
    
    * update: desktop version,: change: scorboard colors and open all accordion menu while clicking anywhere
    
    * add: jersey goal
    
    * add: motm
    
    * update: mvp
    
    * add: last match
    
    * update: when accordion menu is opened at one match, other one will be closed
    
    * update: goal number
    
    * add: SahaSvg component
    
    * add: lucide icons library
    
    * Delete soccer_ball.svg
    
    * add: tab goller and ratings
    
    * add: tabs
    
    * fix: tabs
    
    * fix: scrollbar
    
    ---------
    
    Co-authored-by: Ercode Techoglu <ercode@pm.me>
    Co-authored-by: Ercode Techoglu <me@erkut.dev>

---
+++ src/lib/components/SahaSvg.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+	// export let players = [];
+	export let playersHome = [];
+	export let playersAway = [];
+	export let showPlayerNames = true;
+	export let showPlayerNumbers = true;
+	export let startDrag: (event: PointerEvent | TouchEvent, player: any) => void = () => null;
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 300 500"
+	class="h-full max-h-screen w-full max-w-md"
+>
+	<!-- Çim Alanı -->
+	<rect x="0" y="0" width="300" height="500" fill="green" />
+
+	<!-- Saha Dış Çizgisi -->
+	<rect x="10" y="10" width="280" height="480" fill="none" stroke="white" stroke-width="2" />
+
+	<!-- Orta Çizgi -->
+	<line x1="10" y1="250" x2="290" y2="250" stroke="white" stroke-width="2" />
+
+	<!-- Orta Daire -->
+	<circle cx="150" cy="250" r="70" fill="none" stroke="white" stroke-width="2" />
+	<circle cx="150" cy="250" r="2" fill="white" />
+
+	<!-- Kale Alanları -->
+	<rect x="70" y="10" width="160" height="70" fill="none" stroke="white" stroke-width="2" />
+	<rect x="70" y="420" width="160" height="70" fill="none" stroke="white" stroke-width="2" />
+
+	<!-- Oyuncular Home -->
+	{#each playersHome as player}
+		<g>
+			<!-- Circle representing the player -->
+			<circle
+				class="cursor-move"
+				cx={player.x}
+				cy={player.y}
+				r="10"
+				fill={player.color}
+				aria-label="Oyuncu"
+				role="button"
+				tabindex="0"
+				on:pointerdown={(event) => startDrag(event, player)}
+				on:touchstart={(event) => startDrag(event, player)}
+				style="filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.5));"
+			/>
+
+			<!-- Player's name above the circle -->
+			{#if showPlayerNames}
+				<text
+					class="pointer-events-none font-bold"
+					x={player.x}
+					y={player.y - 15}
+					fill="white"
+					font-size="6"
+					text-anchor="middle"
+					dominant-baseline="middle"
+				>
+					{player.name}
+				</text>
+			{/if}
+
+			<!-- Player's number inside the circle -->
+			{#if showPlayerNumbers}
+				<text
+					class="pointer-events-none font-stretch-semi-expanded"
+					x={player.x}
+					y={player.y + 1}
+					fill="white"
+					font-size="8"
+					text-anchor="middle"
+					dominant-baseline="middle"
+				>
+					{player.number}
+				</text>
+			{/if}
+		</g>
+	{/each}
+
+	<!-- Oyuncular Away -->
+	{#each playersAway as player}
+		<g>
+			<!-- Circle representing the player -->
+			<circle
+				class="cursor-move"
+				cx={player.x}
+				cy={player.y}
+				r="10"
+				fill={player.color}
+				aria-label="Oyuncu"
+				role="button"
+				tabindex="0"
+				on:pointerdown={(event) => startDrag(event, player)}
+				on:touchstart={(event) => startDrag(event, player)}
+				style="filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.5));"
+			/>
+
+			<!-- Player's name above the circle -->
+			{#if showPlayerNames}
+				<text
+					class="pointer-events-none font-bold"
+					x={player.x}
+					y={player.y - 15}
+					fill="white"
+					font-size="6"
+					text-anchor="middle"
+					dominant-baseline="middle"
+				>
+					{player.name}
+				</text>
+			{/if}
+
+			<!-- Player's number inside the circle -->
+			{#if showPlayerNumbers}
+				<text
+					class="pointer-events-none font-stretch-semi-expanded"
+					x={player.x}
+					y={player.y + 1}
+					fill="white"
+					font-size="8"
+					text-anchor="middle"
+					dominant-baseline="middle"
+				>
+					{player.number}
+				</text>
+			{/if}
+		</g>
+	{/each}
+</svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,50 @@
 <script lang="ts">
+	import Saha from "$lib/components/SahaSvg.svelte";
 	let matches = [
+		{
+			homeTeam: "Akkuyu-2023",
+			homeTeamPlayers: ["Eray", "Cansın", "Baki", "Eyüp", "İbrahim", "Hakan"],
+			awayTeam: "Akkuyu-2024",
+			awayTeamPlayers: ["Mete", "Yasin", "Faruk", "Abdullah", "Murat", "Semih"],
+			score: "10 - 10",
+			date: "30 Mart 2025 Pazar",
+			hour: "20:00-21:00",
+			ratingHome: [
+				{ player: "Eray", rating: 5 },
+				{ player: "Cansın", rating: 7 },
+				{ player: "Baki", rating: 8.5 },
+				{ player: "Eyüp", rating: 8 },
+				{ player: "İbrahim", rating: 5.5 },
+				{ player: "Hakan", rating: 7.5 }
+			],
+			goalsHome: [
+				{ player: "Baki", goalNumber: 3 },
+				{ player: "Eyüp", goalNumber: 3 },
+				{ player: "Hakan", goalNumber: 3 },
+				{ player: "İbrahim", goalNumber: 1 }
+			],
+			ratingAway: [
+				{ player: "Mete", rating: 7.5 },
+				{ player: "Yasin", rating: 7.5 },
+				{ player: "Faruk", rating: 8 },
+				{ player: "Abdullah", rating: 7 },
+				{ player: "Murat", rating: 6.5 },
+				{ player: "Semih", rating: 7 }
+			],
+			goalsAway: [
+				{ player: "Mete", goalNumber: 2 },
+				{ player: "Semih", goalNumber: 2 },
+				{ player: "Abdullah", goalNumber: 1 },
+				{ player: "Yasin", goalNumber: 2 },
+				{ player: "Faruk", goalNumber: 2 },
+				{ player: "Murat", goalNumber: 1 }
+			],
+			goalJersey: "-",
+			motm: "Baki",
+			isOpen: false,
+			activeTab: "goals"
+		},
+
 		{
 			homeTeam: "Eray'ın Takımı",
 			homeTeamPlayers: ["Eray", "Baki", "Çağrı", "Eyüp", "İbrahim", "Batuhan"],
@@ -8,11 +53,27 @@
 			score: "8 - 9",
 			date: "28 Mart 2025 Cuma",
 			hour: "21:00-22:00",
+			ratingHome: [
+				{ player: "Eray", rating: 6 },
+				{ player: "Baki", rating: 7 },
+				{ player: "Çağrı", rating: 7 },
+				{ player: "Eyüp", rating: 6.5 },
+				{ player: "İbrahim", rating: 7.5 },
+				{ player: "Batuhan", rating: 5.5 }
+			],
 			goalsHome: [
 				{ player: "Baki", goalNumber: 2 },
 				{ player: "Çağrı", goalNumber: 2 },
 				{ player: "Eyüp", goalNumber: 1 },
 				{ player: "İbrahim", goalNumber: 3 }
+			],
+			ratingAway: [
+				{ player: "Erkut", rating: 6.5 },
+				{ player: "Yasin", rating: 8.5 },
+				{ player: "Faruk", rating: 8 },
+				{ player: "Cansın", rating: 7 },
+				{ player: "Velid", rating: 9.5 },
+				{ player: "Semih", rating: 6 }
 			],
 			goalsAway: [
 				{ player: "Velid", goalNumber: 3 },
@@ -23,7 +84,8 @@
 			],
 			goalJersey: "Velid",
 			motm: "Velid",
-			isOpen: false
+			isOpen: false,
+			activeTab: "goals"
 		}
 	];
 
@@ -41,50 +103,53 @@
 			return goalsB - goalsA;
 		});
 	});
+
+	// Function to toggle the accordion state
+	function toggleAccordion(index: number) {
+		matches = matches.map((match, i) => ({
+			...match,
+			isOpen: i === index ? !match.isOpen : false, // Close others, toggle the clicked one
+			activeTab: i === index && !match.isOpen ? "goals" : match.activeTab // Reset tab if closing
+		}));
+	}
 </script>
 
 <div class="flex flex-col items-center gap-6 px-4 sm:px-8 lg:px-31">
-	{#each matches as match}
+	{#each matches as match, index}
 		<!-- svelte-ignore a11y_click_events_have_key_events -->
 		<!-- svelte-ignore a11y_no_static_element_interactions -->
 		<div
 			class="card bg-base-300 w-full max-w-4xl cursor-pointer shadow-xl"
-			on:click={() => (match.isOpen = !match.isOpen)}
+			on:click={() => toggleAccordion(index)}
 		>
 			<div class="card-body">
 				<h2 class="card-title text-primary">{match.date}</h2>
 				<p class="text-success text-xs">{match.hour}</p>
 
-				<div class="flex flex-wrap items-center justify-between">
+				{#if match.isOpen}
+					<!-- Tab Menu -->
+					<div class="tabs tabs-boxed mt-4 flex justify-center">
+						<div
+							class="tab {match.activeTab === 'goals' ? 'tab-active' : ''}"
+							on:click|stopPropagation={() => (match.activeTab = "goals")}
+						>
+							Goller
+						</div>
+						<div
+							class="tab {match.activeTab === 'ratings' ? 'tab-active' : ''}"
+							on:click|stopPropagation={() => (match.activeTab = "ratings")}
+						>
+							Ratingler
+						</div>
+					</div>
+				{/if}
+
+				<div class="mt-4 flex flex-wrap items-center justify-between">
 					<!-- Home Team Section -->
 					<div class="flex min-h-[20px] w-full flex-col items-center sm:w-1/3">
 						<div class="collapse w-full" class:collapse-open={match.isOpen}>
 							<div class="collapse-title p-3.5 text-center text-sm font-semibold">
 								<span class="badge badge-primary mt-1">{match.homeTeam}</span>
-							</div>
-							<div class="collapse-content p-2">
-								<ul class="list-none text-center text-sm leading-tight">
-									{#each match.homeTeamPlayers as player}
-										<li class="flex items-center gap-2">
-											<span class="text-primary">{player}</span>
-											{#if match.motm === player}
-												<span>👑</span>
-											{/if}
-											{#if match.goalsHome.find((goal) => goal.player === player)}
-												<div class="flex w-full justify-end text-right">
-													{#if match.goalJersey === player}
-														<span>🎽</span>
-													{/if}
-													{#each Array(match.goalsHome.find((goal) => goal.player === player)?.goalNumber ?? 0).fill(null) as _}
-														⚽
-													{/each}
-												</div>
-											{/if}
-										</li>
-									{/each}
-									<!-- Boşluk için eklenen boş li -->
-									<li class="min-h-[20px]"></li>
-								</ul>
 							</div>
 						</div>
 					</div>
@@ -104,39 +169,95 @@
 							<div class="collapse-title p-3.5 text-center text-sm font-semibold">
 								<span class="badge badge-secondary mt-1">{match.awayTeam}</span>
 							</div>
-							<div class="collapse-content relative p-2">
-								<ul class="list-none text-center text-sm leading-tight">
-									{#each match.awayTeamPlayers as player}
-										<li class="flex items-center gap-2">
-											<span class="text-secondary">{player}</span>
-											{#if match.motm === player}
-												<span>👑</span>
-											{/if}
-											<div class="flex w-full justify-end text-right">
-												{#if match.goalsAway.find((goal) => goal.player === player)}
-													{#if match.goalJersey === player}
-														<span>🎽</span>
-													{/if}
-													{#each Array(match.goalsAway.find((goal) => goal.player === player)?.goalNumber || 0).fill(null) as _}
-														⚽
-													{/each}
-												{/if}
-											</div>
-										</li>
-									{/each}
-									<!-- Boşluk için eklenen boş li -->
-									<li class="min-h-[20px]"></li>
-								</ul>
-								{#if match.isOpen}
-									<div class="absolute right-0.5 bottom-0.5 text-[11px]">
-										🎽: Forma golü<br />
-										👑: Maçın Adamı
-									</div>
-								{/if}
-							</div>
 						</div>
 					</div>
 				</div>
+
+				{#if match.isOpen}
+					<div>
+						<!-- Goller Tabı -->
+						{#if match.activeTab === "goals"}
+							<div>
+								<!-- Mevcut goller ekranı burada kalıyor -->
+								<div class="flex flex-wrap items-center justify-between">
+									<!-- Home Team Section -->
+									<div class="flex min-h-[20px] w-full flex-col items-center sm:w-1/3">
+										<div class="collapse w-full" class:collapse-open={match.isOpen}>
+											<div class="collapse-content p-2">
+												<ul class="list-none text-center text-sm leading-tight">
+													{#each match.homeTeamPlayers as player}
+														<li class="flex items-center gap-2">
+															<span class="text-primary">{player}</span>
+															{#if match.motm === player}
+																<span>👑</span>
+															{/if}
+															{#if match.goalsHome.find((goal) => goal.player === player)}
+																<div class="flex w-full justify-end text-right">
+																	{#if match.goalJersey === player}
+																		<span>🎽</span>
+																	{/if}
+																	{#each Array(match.goalsHome.find((goal) => goal.player === player)?.goalNumber ?? 0).fill(null) as _}
+																		⚽
+																	{/each}
+																</div>
+															{/if}
+														</li>
+													{/each}
+													<!-- Boşluk için eklenen boş li -->
+													<li class="min-h-[20px]"></li>
+												</ul>
+											</div>
+										</div>
+									</div>
+
+									<!-- Away Team Section -->
+									<div class="flex min-h-[20px] w-full flex-col items-center sm:w-1/3">
+										<div class="collapse w-full" class:collapse-open={match.isOpen}>
+											<div class="collapse-content relative p-2">
+												<ul class="list-none text-center text-sm leading-tight">
+													{#each match.awayTeamPlayers as player}
+														<li class="flex items-center gap-2">
+															<span class="text-secondary">{player}</span>
+															{#if match.motm === player}
+																<span>👑</span>
+															{/if}
+															<div class="flex w-full justify-end text-right">
+																{#if match.goalsAway.find((goal) => goal.player === player)}
+																	{#if match.goalJersey === player}
+																		<span>🎽</span>
+																	{/if}
+																	{#each Array(match.goalsAway.find((goal) => goal.player === player)?.goalNumber || 0).fill(null) as _}
+																		⚽
+																	{/each}
+																{/if}
+															</div>
+														</li>
+													{/each}
+													<!-- Boşluk için eklenen boş li -->
+													<li class="min-h-[20px]"></li>
+												</ul>
+												{#if match.isOpen}
+													<div class="absolute right-0.5 bottom-0.5 text-[11px]">
+														🎽: Forma golü<br />
+														👑: Maçın Adamı
+													</div>
+												{/if}
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						{/if}
+
+						<!-- Ratingler Tabı -->
+						{#if match.activeTab === "ratings"}
+							<div class="flex h-96 items-center justify-center">
+								<!-- Centered Saha component -->
+								<Saha />
+							</div>
+						{/if}
+					</div>
+				{/if}
 			</div>
 		</div>
 	{/each}
