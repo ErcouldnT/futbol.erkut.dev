3b420d9818c9eef84044c63d499f88d216b345a5
Semih <75166674+SemihBolukbasi@users.noreply.github.com>
2025-04-01 19:42:30 +0300

    add: mvp and forma golü  (#8)
    * change: skoreboard, teams, goals
    
    * Update supabase.ts
    
    Production secret lar local secret tan farklı
    
    * update: mobile version
    
    * change: goaler players
    
    * format: string ler tek tırnak yerine çift tırnak
    
    * update: desktop version,: change: scorboard colors and open all accordion menu while clicking anywhere
    
    * add: jersey goal
    
    * add: motm
    
    * update: mvp
    
    ---------
    
    Co-authored-by: Ercode Techoglu <ercode@pm.me>
    Co-authored-by: Ercode Techoglu <me@erkut.dev>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,10 @@
 				{ player: "Erkut", goalNumber: 1 },
 				{ player: "Yasin", goalNumber: 2 },
 				{ player: "Faruk", goalNumber: 1 }
-			]
+			],
+			goalJersey: "Velid",
+			motm: "Velid",
+			isOpen: false
 		}
 	];
 
@@ -42,15 +45,20 @@
 
 <div class="flex flex-col items-center gap-6 px-4 sm:px-8 lg:px-31">
 	{#each matches as match}
-		<div class="card bg-base-300 w-full max-w-4xl cursor-pointer shadow-xl">
+		<!-- svelte-ignore a11y_click_events_have_key_events -->
+		<!-- svelte-ignore a11y_no_static_element_interactions -->
+		<div
+			class="card bg-base-300 w-full max-w-4xl cursor-pointer shadow-xl"
+			on:click={() => (match.isOpen = !match.isOpen)}
+		>
 			<div class="card-body">
 				<h2 class="card-title text-primary">{match.date}</h2>
 				<p class="text-success text-xs">{match.hour}</p>
-				<div class="flex flex-wrap items-start justify-between">
+
+				<div class="flex flex-wrap items-center justify-between">
 					<!-- Home Team Section -->
-					<div class="flex min-h-[20px] w-full flex-col items-center sm:w-1/2 lg:w-1/4">
-						<div class="collapse-arrow collapse w-full">
-							<input type="checkbox" class="peer" />
+					<div class="flex min-h-[20px] w-full flex-col items-center sm:w-1/3">
+						<div class="collapse w-full" class:collapse-open={match.isOpen}>
 							<div class="collapse-title p-3.5 text-center text-sm font-semibold">
 								<span class="badge badge-primary mt-1">{match.homeTeam}</span>
 							</div>
@@ -59,57 +67,72 @@
 									{#each match.homeTeamPlayers as player}
 										<li class="flex items-center gap-2">
 											<span class="text-primary">{player}</span>
+											{#if match.motm === player}
+												<span>👑</span>
+											{/if}
 											{#if match.goalsHome.find((goal) => goal.player === player)}
 												<div class="flex w-full justify-end text-right">
+													{#if match.goalJersey === player}
+														<span>🎽</span>
+													{/if}
 													{#each Array(match.goalsHome.find((goal) => goal.player === player)?.goalNumber ?? 0).fill(null) as _}
-														<!-- <img
-														src="/soccer_ball.svg"
-														alt="Goal Ball"
-														class="text-primary h-4 w-4"
-													/> -->
 														⚽
 													{/each}
 												</div>
 											{/if}
 										</li>
 									{/each}
+									<!-- Boşluk için eklenen boş li -->
+									<li class="min-h-[20px]"></li>
 								</ul>
 							</div>
 						</div>
 					</div>
 
-					<!-- Match Score Section -->
-					<div class="mt-2 flex w-full flex-col items-center text-center sm:w-1/2 lg:w-1/4">
-						<p class="text-4xl font-bold">{match.score}</p>
+					<!-- Scoreboard Section -->
+					<div class="flex w-full flex-col items-center justify-center sm:w-1/4">
+						<p class="text-4xl font-bold">
+							<span class="text-primary">{match.score.split(" - ")[0]}</span>
+							<span> - </span>
+							<span class="text-secondary">{match.score.split(" - ")[1]}</span>
+						</p>
 					</div>
 
 					<!-- Away Team Section -->
-					<div class="flex min-h-[20px] w-full flex-col items-center sm:w-1/2 lg:w-1/4">
-						<div class="collapse-arrow collapse w-full">
-							<input type="checkbox" class="peer" />
+					<div class="flex min-h-[20px] w-full flex-col items-center sm:w-1/3">
+						<div class="collapse w-full" class:collapse-open={match.isOpen}>
 							<div class="collapse-title p-3.5 text-center text-sm font-semibold">
 								<span class="badge badge-secondary mt-1">{match.awayTeam}</span>
 							</div>
-							<div class="collapse-content p-2">
+							<div class="collapse-content relative p-2">
 								<ul class="list-none text-center text-sm leading-tight">
 									{#each match.awayTeamPlayers as player}
 										<li class="flex items-center gap-2">
 											<span class="text-secondary">{player}</span>
-											{#if match.goalsAway.find((goal) => goal.player === player)}
-												<div class="flex w-full justify-end text-right">
+											{#if match.motm === player}
+												<span>👑</span>
+											{/if}
+											<div class="flex w-full justify-end text-right">
+												{#if match.goalsAway.find((goal) => goal.player === player)}
+													{#if match.goalJersey === player}
+														<span>🎽</span>
+													{/if}
 													{#each Array(match.goalsAway.find((goal) => goal.player === player)?.goalNumber || 0).fill(null) as _}
-														<!-- <img
-														src="/soccer_ball.svg"
-														alt="Goal Ball"
-														class="text-secondary h-4 w-4"
-													/> -->
 														⚽
 													{/each}
-												</div>
-											{/if}
+												{/if}
+											</div>
 										</li>
 									{/each}
+									<!-- Boşluk için eklenen boş li -->
+									<li class="min-h-[20px]"></li>
 								</ul>
+								{#if match.isOpen}
+									<div class="absolute right-0.5 bottom-0.5 text-[11px]">
+										🎽: Forma golü<br />
+										👑: Maçın Adamı
+									</div>
+								{/if}
 							</div>
 						</div>
 					</div>
