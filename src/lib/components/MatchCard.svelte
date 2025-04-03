3a9867f6aa33be7dee507d877b028962ff3e7eee
Ercode Techoglu <ercode@pm.me>
2025-04-03 14:28:58 +0300

    Update MatchCard.svelte

--- src/lib/components/MatchCard.svelte
+++ src/lib/components/MatchCard.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import type { MatchWithTeams } from "$lib/types";
+	import { ArrowDownNarrowWide } from "@lucide/svelte";
 	import TabMenu from "./TabMenu.svelte";
 
 	export let match: MatchWithTeams;
 	export let index: number;
 	export let lastMatch = false;
+
+	console.log(index + 1); // for future
 
 	let isExpanded = false;
 
@@ -36,28 +39,32 @@
 </script>
 
 <main class="card bg-base-300 w-full p-5">
-	{#if match.match_time}
-		<div class="flex items-center justify-between">
-			<p class="text-primary text-lg font-bold">
-				{new Date(match.match_time).toLocaleDateString("tr-TR", {
-					day: "2-digit",
-					month: "long",
-					year: "numeric"
-				})}
-			</p>
-			<button on:click={toggleAccordion} class="btn font-thin opacity-50"
-				>Maç Detayları #{index + 1}</button
-			>
-		</div>
-		<p class="text-success text-xs">{formatMatchTime(new Date(match.match_time))}</p>
-	{/if}
+	<!-- svelte-ignore a11y_click_events_have_key_events -->
+	<!-- svelte-ignore a11y_no_static_element_interactions -->
+	<div on:click={toggleAccordion} class="cursor-pointer">
+		{#if match.match_time}
+			<div class="flex items-center justify-between">
+				<p class="text-primary text-lg font-bold">
+					{new Date(match.match_time).toLocaleDateString("tr-TR", {
+						day: "2-digit",
+						month: "long",
+						year: "numeric"
+					})}
+				</p>
+				<button class="btn font-thin opacity-50"
+					>Maç Detayları <ArrowDownNarrowWide size={18} /></button
+				>
+			</div>
+			<p class="text-success text-xs">{formatMatchTime(new Date(match.match_time))}</p>
+		{/if}
 
-	<div class="grid grid-cols-3 items-center justify-items-center p-5">
-		<p class="badge badge-primary font-bold">{match.team_1.name}</p>
-		<div>
-			<p class="text-4xl font-extrabold">{match.home_score} - {match.away_score}</p>
+		<div class="grid grid-cols-3 items-center justify-items-center p-5">
+			<p class="badge badge-primary font-bold">{match.team_1.name}</p>
+			<div>
+				<p class="text-4xl font-extrabold">{match.home_score} - {match.away_score}</p>
+			</div>
+			<p class="badge badge-secondary font-bold">{match.team_2.name}</p>
 		</div>
-		<p class="badge badge-secondary font-bold">{match.team_2.name}</p>
 	</div>
 
 	<!-- <div class="flex items-center justify-center gap-5 opacity-50">
