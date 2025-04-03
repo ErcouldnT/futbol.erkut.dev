2b9be5d6dacea83622e359060a77a07ff2df608b
Semih <semih.bolukbasi@yandex.com>
2025-04-03 15:03:24 +0300

    Update MatchCard.svelte

--- src/lib/components/MatchCard.svelte
+++ src/lib/components/MatchCard.svelte
@@ -38,26 +38,30 @@
 <main class="card bg-base-300 w-full p-5">
 	{#if match.match_time}
 		<div class="flex items-center justify-between">
-			<p class="text-primary text-lg font-bold">
+			<p class="text-primary text-sm font-bold sm:text-lg">
 				{new Date(match.match_time).toLocaleDateString("tr-TR", {
 					day: "2-digit",
 					month: "long",
 					year: "numeric"
 				})}
 			</p>
-			<button on:click={toggleAccordion} class="btn font-thin opacity-50"
+			<button on:click={toggleAccordion} class="btn btn-sm sm:btn-md font-thin opacity-50"
 				>Maç Detayları #{index + 1}</button
 			>
 		</div>
-		<p class="text-success text-xs">{formatMatchTime(new Date(match.match_time))}</p>
+		<p class="text-success text-xs sm:text-base">
+			{formatMatchTime(new Date(match.match_time))}
+		</p>
 	{/if}
 
-	<div class="grid grid-cols-3 items-center justify-items-center p-5">
-		<p class="badge badge-primary font-bold">{match.team_1.name}</p>
+	<div class="my-6 grid grid-cols-3 items-center justify-items-center p-1 sm:p-3">
+		<p class="bg-primary rounded-xl p-1 px-2 text-xs font-bold sm:text-base">{match.team_1.name}</p>
 		<div>
-			<p class="text-4xl font-extrabold">{match.home_score} - {match.away_score}</p>
+			<p class="text-sm font-extrabold sm:text-4xl">{match.home_score} - {match.away_score}</p>
 		</div>
-		<p class="badge badge-secondary font-bold">{match.team_2.name}</p>
+		<p class="bg-secondary rounded-xl p-1 px-2 text-xs font-bold sm:text-base">
+			{match.team_2.name}
+		</p>
 	</div>
 
 	<!-- <div class="flex items-center justify-center gap-5 opacity-50">
